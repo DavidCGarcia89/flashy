f89997316246d0854a453142ba913481ce542d8d
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 01:21:01 +0200

    comillas simples

--- src/python-scripts/motors.py
+++ src/python-scripts/motors.py
@@ -4,16 +4,15 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
  
 Motor2A = 4
-
  
 GPIO.setup(Motor2A,GPIO.OUT)
 
-print("Turning light on")
+print('Turning light on')
 GPIO.output(Motor2A,GPIO.HIGH)
  
 sleep(2)
  
-print("Turning light off")
+print('Turning light off')
 GPIO.output(Motor2A,GPIO.LOW)
 
 GPIO.cleanup()
