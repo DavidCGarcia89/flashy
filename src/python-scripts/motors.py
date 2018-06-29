865351d9c21928ee57edd849c25b80c4f0d26ad0
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 01:09:54 +0200

    Añadido funcionalidad pin

--- src/python-scripts/motors.py
+++ src/python-scripts/motors.py
@@ -1,3 +1,18 @@
-f = open("./testing.txt","w+")
-f.write('Python se ha ejecutado correctamente creando el fichero testing')
-f.close()
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+Motor1A = 16
+ 
+GPIO.setup(Motor1A,GPIO.OUT)
+
+print "Turning light on"
+GPIO.output(Motor1A,GPIO.HIGH)
+ 
+sleep(2)
+ 
+print "Turning light off"
+GPIO.output(Motor1E,GPIO.LOW)
+ 
+GPIO.cleanup()
