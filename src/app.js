fb1dbd0af5cb75b5f0adeae03ab9db523faa1e6e
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 00:44:42 +0200

    Cmabios en el cmd get

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ var cmd = require ("node-cmd");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-  /*cmd.get(
-      'py ./python-scripts/motors.py',
-      function(err, data, stderr){
-          if (!err) {
-             console.log('the node-cmd cloned dir contains these files :\n\n',data)
-          } else {
-             console.log('error', err)
-          }
-      }
-  )*/
+  //res.send('Hello World!');
+  var pyProcess = cmd.get('python ./public/algoritmo/munkresAlgorithm.py',
+        function(data, err, stderr) {
+            if (!err) {
+              console.log("Llamada a python correcta")
+            } 
+            else {
+                console.log("python script cmd error: " + err)
+            }
+        }
+    );
 });
 
 app.listen(3000, function () {
--- src/python-scripts/motors.py
+++ src/python-scripts/motors.py
@@ -1,1 +1,2 @@
 #saiodh
+print("Hello World")
