dae3ae11578556b02857d859c52a9fc785623eb4
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 01:15:27 +0200

    p2.7 a p3

--- src/python-scripts/motors.py
+++ src/python-scripts/motors.py
@@ -8,12 +8,12 @@ Motor2A = 7
  
 GPIO.setup(Motor2A,GPIO.OUT)
 
-print "Turning light on"
+print("Turning light on")
 GPIO.output(Motor2A,GPIO.HIGH)
  
 sleep(2)
  
-print "Turning light off"
+print("Turning light off")
 GPIO.output(Motor2A,GPIO.LOW)
 
 GPIO.cleanup()
