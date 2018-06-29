9a6b72d6a581dd0c422aeac333e7f7a7a2644ebb
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 01:52:33 +0200

    asd

--- src/python-scripts/motors.py
+++ src/python-scripts/motors.py
@@ -2,17 +2,17 @@ import RPi.GPIO as GPIO
 from time import sleep
  
 GPIO.setmode(GPIO.BCM)
-for x in range(26, 38):
-    Motor2A = x
-    
-    GPIO.setup(Motor2A,GPIO.OUT)
 
-    print('Turning light on')
-    print(x)
-    GPIO.output(Motor2A,GPIO.HIGH)
-    
-    sleep(1)
-    
-    print('Turning light off')
-    GPIO.output(Motor2A,GPIO.LOW)   
+Motor2A = sys.argv[1]
+
+GPIO.setup(Motor2A,GPIO.OUT)
+
+print('Turning light on')
+
+GPIO.output(Motor2A,GPIO.HIGH)
+
+sleep(1)
+
+print('Turning light off')
+GPIO.output(Motor2A,GPIO.LOW)   
 GPIO.cleanup()
