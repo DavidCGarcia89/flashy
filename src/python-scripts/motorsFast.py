8712da14d1c7809f50c831f5859dac8262968614
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 03:01:55 +0200

    asd

--- src/python-scripts/motorsFast.py
+++ src/python-scripts/motorsFast.py
@@ -3,7 +3,7 @@ from time import sleep
 import sys
  
 GPIO.setmode(GPIO.BCM)
-
+print(sys.argc)
 if (sys.argc != 4):
     print("Uso: python3 motorsFast.py #Pin #ciclosBucle #tiempoEntreEncedidoyApagado")
     exit(1)
@@ -14,7 +14,7 @@ tiempo = int(sys.argv[3])
 print(Motor2A)
 
 GPIO.setup(Motor2A,GPIO.OUT)
-for x in range(bucle)
+for x in range(bucle):
     GPIO.output(Motor2A,GPIO.HIGH)
     sleep(0.1)
     GPIO.output(Motor2A,GPIO.LOW)   
