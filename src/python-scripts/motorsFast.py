0e931913a00dae354b1d03b301217b0bf7eefa2a
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 03:11:15 +0200

    ads

--- src/python-scripts/motorsFast.py
+++ src/python-scripts/motorsFast.py
@@ -4,18 +4,20 @@ import sys
  
 GPIO.setmode(GPIO.BCM)
 
-if (len(sys.argv) != 4):
-    print("Uso: python3 motorsFast.py #Pin #ciclosBucle #tiempoEntreEncedidoyApagado")
+if (len(sys.argv) != 5):
+    print("Uso: python3 motorsFast.py #Pin #ciclosBucle #tiempoEntreEncedidoyApagado #porcentaje(0-1)")
     exit(1)
 Motor2A = int(sys.argv[1])
 bucle = int(sys.argv[2])
 tiempo = float(sys.argv[3])
+porcentaje = float(sys.arv[4])
 
 print(Motor2A)
 
 GPIO.setup(Motor2A,GPIO.OUT)
-for x in range(bucle):
+for x in range(bucle*1000):
     GPIO.output(Motor2A,GPIO.HIGH)
-    sleep(tiempo)
-    GPIO.output(Motor2A,GPIO.LOW)   
+    sleep(tiempo*porcentaje/1000)
+    GPIO.output(Motor2A,GPIO.LOW) 
+    sleep(tiempo*(1-porcentaje)/1000)  
 GPIO.cleanup()
