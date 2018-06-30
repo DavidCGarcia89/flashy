dc9818a2c1502bbf3b7e0e93a66b583ce9e20b3c
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 02:59:19 +0200

    asd

---
+++ src/python-scripts/motorsFast.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import sys
+ 
+GPIO.setmode(GPIO.BCM)
+
+if (sys.argc != 4)
+    print("Uso: python3 motorsFast.py #Pin #ciclosBucle #tiempoEntreEncedidoyApagado")
+Motor2A = int(sys.argv[1])
+bucle = int(sys.argv[2])
+tiempo = int(sys.argv[3])
+
+print(Motor2A)
+
+GPIO.setup(Motor2A,GPIO.OUT)
+for x in range(bucle)
+    GPIO.output(Motor2A,GPIO.HIGH)
+    sleep(0.1)
+    GPIO.output(Motor2A,GPIO.LOW)   
+GPIO.cleanup()
