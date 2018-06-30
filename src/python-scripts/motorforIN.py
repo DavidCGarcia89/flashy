10e47b62021509a88e730cc7ad8eb333e588e711
David Campanero Garcia <cgarciadavid@gmail.com>
2018-06-30 02:28:39 +0200

    limpiar

---
+++ src/python-scripts/motorforIN.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import sys
+ 
+GPIO.setmode(GPIO.BCM)
+
+for x in range(1,27):
+    Motor2A = x
+    print(Motor2A)
+    GPIO.setup(Motor2A,GPIO.OUT)
+
+for x in range(1,27):
+    print('Turning light on')
+    GPIO.output(Motor2A,GPIO.HIGH)
+
+sleep(2)
+
+for x in range (1,27):
+    print('Turning light off')
+    .output(Motor2A,GPIO.LOW)
+
+GPIO.cleanup()
