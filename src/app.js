198d166ae18fd93f1ad868d4b2ffe85d79b397d7
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-04 01:21:25 +0200

    asd

--- src/app.js
+++ src/app.js
@@ -4,22 +4,7 @@ var cmd = require ("node-cmd");
 var app = express();
 
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
 });
 
 app.get('/', function (req, res) {
@@ -37,19 +22,19 @@ app.get('/raspberry', function (req, res) {
                 function(data, err, stderr) {
                     if (!err) {
                     console.log("¡Llamada con éxito!")
-                    res.send("Ok");//¡Llamada con éxito!
+                    res.send('Ok');//¡Llamada con éxito!
                     } 
                     else {
                         console.log("python script cmd error: " + err)
-                        res.send("Error");//Hubo un error en la raspberry
+                        res.send('Error');//Hubo un error en la raspberry
                     }
                 }
             );
         } else {
-            res.send("Pin Incorrecto");//"El pin debe estar entre el 2 y el 26"
+            res.send('Pin Incorrecto');//"El pin debe estar entre el 2 y el 26"
         }
     } else {
-        res.send("Sin pin");//Se debe definir el pin
+        res.send('Sin pin');//Se debe definir el pin
     }
 });
 
