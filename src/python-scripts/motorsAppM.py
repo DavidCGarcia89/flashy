1d30f5662da04b4110e444fb66b6c9c0ff2cb152
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-07 00:42:32 +0200

    asd

--- src/python-scripts/motorsAppM.py
+++ src/python-scripts/motorsAppM.py
@@ -5,18 +5,23 @@ GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
 GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
 p = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
 p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
-print("Comenzamos el bucle")
-try:                 
-    while True:      #iniciamos un loop infinito
-        print("Estamos en el bucle")
-        # p.ChangeDutyCycle(4.5)    #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
-        # time.sleep(0.5)           #pausa de medio segundo
-        p.ChangeDutyCycle(10.5)   #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
-        time.sleep(0.5)           #pausa de medio segundo
+t_end = time.time() + 10
+               
+while time.time() < t_end:      #iniciamos un loop
+    p.ChangeDutyCycle(4.5)      #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+                                #pausa de medio segundo
+t_end = time.time() + 10             
+while time.time() < t_end:      #iniciamos un loop
+    p.ChangeDutyCycle(10.5)     #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+                                    #pausa de medio segundo
         # p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
         # time.sleep(0.5)           #pausa de medio segundo
+t_end = time.time() + 10
+cycle = 10.5  
+while time.time() < t_end or cycle > 4.5: 
+    p.ChangeDutyCycle(cycle)
+    cycle = cycle - 0.01
 
-except KeyboardInterrupt:
-    print("Paramos el bucle")         #Si el usuario pulsa CONTROL+C entonces...
-    p.stop()                      #Detenemos el servo 
-    GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
+print("Paramos el bucle")         #Si el usuario pulsa CONTROL+C entonces...
+p.stop()                      #Detenemos el servo 
+GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
