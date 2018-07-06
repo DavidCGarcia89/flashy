99bcdb0cda3f99f7b9bdd10e84f0f847bfec44ff
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-06 21:09:35 +0200

    BOARD TO BCM

--- src/python-scripts/motorsApp.py
+++ src/python-scripts/motorsApp.py
@@ -18,7 +18,7 @@
 import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
 
-GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
+GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
 GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
 p = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
 p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
