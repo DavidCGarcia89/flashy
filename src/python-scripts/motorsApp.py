97d5cc3a1fbaba95a8afbb3caabc91b1c65ad6a5
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-06 20:46:58 +0200

    asd

--- src/python-scripts/motorsApp.py
+++ src/python-scripts/motorsApp.py
@@ -1,16 +1,38 @@
-import RPi.GPIO as GPIO
-from time import sleep
-import sys
+# import RPi.GPIO as GPIO
+# from time import sleep
+# import sys
  
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
-gpioPin = int(sys.argv[1])
+# gpioPin = int(sys.argv[1])
 
-GPIO.setup(gpioPin,GPIO.OUT)
+# GPIO.setup(gpioPin,GPIO.OUT)
 
-GPIO.output(gpioPin,GPIO.HIGH)
+# GPIO.output(gpioPin,GPIO.HIGH)
 
-sleep(2)
+# sleep(2)
 
-GPIO.output(gpioPin,GPIO.LOW)   
-GPIO.cleanup()
+# GPIO.output(gpioPin,GPIO.LOW)   
+# GPIO.cleanup()
+
+import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
+import time                #Importamos time para poder usar time.sleep
+
+GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
+GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
+p = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
+p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
+
+try:                 
+    while True:      #iniciamos un loop infinito
+
+        p.ChangeDutyCycle(4.5)    #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+        time.sleep(0.5)           #pausa de medio segundo
+        p.ChangeDutyCycle(10.5)   #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+        time.sleep(0.5)           #pausa de medio segundo
+        p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
+        time.sleep(0.5)           #pausa de medio segundo
+
+except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
+    p.stop()                      #Detenemos el servo 
+    GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
