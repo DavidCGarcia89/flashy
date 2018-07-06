745a3b75ced529d2c7c2cd7f1277f60328633d5f
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-07 01:07:37 +0200

    asd

--- src/python-scripts/motorsAppM.py
+++ src/python-scripts/motorsAppM.py
@@ -5,25 +5,25 @@ GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
 GPIO.setup(21,GPIO.OUT)    #Ponemos el pin 21 como salida
 p = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
 p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
-t_end = time.time() + 5
+# t_end = time.time() + 5
                
-while time.time() < t_end:      #iniciamos un loop
-    p.ChangeDutyCycle(4.5)      #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
-                                #pausa de medio segundo
-t_end = time.time() + 5             
-while time.time() < t_end:      #iniciamos un loop
-    p.ChangeDutyCycle(10.5)     #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
+# while time.time() < t_end:      #iniciamos un loop
+#     p.ChangeDutyCycle(4.5)      #Enviamos un pulso del 4.5% para girar el servo hacia la izquierda
+#                                 #pausa de medio segundo
+# t_end = time.time() + 5             
+# while time.time() < t_end:      #iniciamos un loop
+#     p.ChangeDutyCycle(10.5)     #Enviamos un pulso del 10.5% para girar el servo hacia la derecha
                                     #pausa de medio segundo
         # p.ChangeDutyCycle(7.5)    #Enviamos un pulso del 7.5% para centrar el servo de nuevo
         # time.sleep(0.5)           #pausa de medio segundo
 print("centrar")
-p.ChangeDutyCycle(5.5)
+p.ChangeDutyCycle(5.55)
 time.sleep(5)
 print("termina la espera")
 d=15
 t_end = time.time() + d
-cycle_start = 6
-cycle_end = 5
+cycle_start = 5.7
+cycle_end = 5.5
 cycle = cycle_start
 while time.time() < t_end: 
     print(cycle)
