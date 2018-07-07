a397735188ab87cfc625f0ecb122afaace719c55
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-07 15:34:53 +0200

    asd

--- src/python-scripts/checkServo.py
+++ src/python-scripts/checkServo.py
@@ -2,9 +2,10 @@ import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
 import sys
 
+pin = sys.argv[1] or 21
 GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
-GPIO.setup(sys.argv[1],GPIO.OUT)    #Ponemos el pin 21 como salida
-p = GPIO.PWM(21,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
+GPIO.setup(pin,GPIO.OUT)    #Ponemos el pin 21 como salida
+p = GPIO.PWM(pin,50)        #Ponemos el pin 21 en modo PWM y enviamos 50 pulsos por segundo
 p.start(7.5)            #Enviamos un pulso del 7.5% para centrar el servo
 duracionGiroInicial = sys.argv[2] or 5
 duracionGiroContrario = sys.argv[3] or 5
