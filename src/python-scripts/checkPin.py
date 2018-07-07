9273cec06a47d97a16e1e2d4c962a08d211ef1db
David Campanero Garcia <cgarciadavid@gmail.com>
2018-07-07 20:26:05 +0200

    asd

--- src/python-scripts/checkPin.py
+++ src/python-scripts/checkPin.py
@@ -5,16 +5,12 @@ import sys
 GPIO.setmode(GPIO.BCM)
 
 Motor2A = int(sys.argv[1])
-print(Motor2A)
 
 GPIO.setup(Motor2A,GPIO.OUT)
-
-print('Turning light on')
 
 GPIO.output(Motor2A,GPIO.HIGH)
 
 sleep(2)
 
-print('Turning light off')
 GPIO.output(Motor2A,GPIO.LOW)   
 GPIO.cleanup()
